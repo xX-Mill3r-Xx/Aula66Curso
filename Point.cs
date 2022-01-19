416a8109a21a388d0d92b7b3da2b20089b36288d
xX-Mill3r-Xx <millermartinsvasco1@gmail.com>
2022-01-19 15:20:56 -0300

    Adicionar arquivos de projeto.

---
+++ Point.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula66Curso
+{
+    struct Point
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula66Curso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p;
+
+            p.X = 10;
+            p.Y = 20;
+            Console.WriteLine(p);
+
+            p = new Point();
+            Console.WriteLine(p);
+
+            Console.ReadLine();
+        }
+    }
+}
